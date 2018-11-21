a5d46cdb1a9420ba70c0bcea01d388f02b6875c1
Justin Booth <jbooth@simpleviewinc.com>
2018-11-20 17:28:06 -0700

    Nav commit

--- js/nav.js
+++ js/nav.js
@@ -5,12 +5,6 @@
 
 // Mobile Nav
 
-     $('body').on('click','.hamburger', function() {
-        $(this).toggleClass('active');
-        $(this).parent().toggleClass('active');
-        $('body').find('[data-sv-mainnav]').toggleClass('active');
-    });
-
 
      $('body').on('click','[data-menu-toggle-parent]',function(){
         $(this).toggleClass('active');
