dcaba8dcf34c092eac4a4f77d0b208dcbbfb82ad
Justin Booth <jbooth@simpleviewinc.com>
2018-11-28 17:12:52 -0700

    End of the line

--- js/nav.js
+++ js/nav.js
@@ -7,37 +7,13 @@
 
 
      $('body').on('click','[data-menu-toggle-parent]',function(){
-        $(this).toggleClass('active');
-        $(this).parent().find('.mobile-children').slideToggle();
-        $(this).parent().find('.parent-arrow').toggleClass('open');
-        $(this).parent().toggleClass('selected');
-        if($(this).parent().find('.parent-arrow').hasClass('open') || $(this).parent().siblings().find('.parent-arrow').hasClass('open')){
-            add_scroll_to_mobile();
-        }else{
-            remove_scroll();
-        }
+        $(this).parent().parent().addClass("active");
     });
 
-     $('body').on('click','[data-menu-toggle-item]',function(){
-        $(this).next().slideToggle();
-        $(this).toggleClass('open');
-        $(this).prev('div').toggleClass('selected');
+    $("body").on("click", ".close-submenu", function(){
+        $(this).parent().parent().removeClass("active");
     });
 
-    function add_scroll_to_mobile(){
-        var $nav =  $('body').find('[data-sv-mainnav]');
-        var height = $( window ).height();
-
-        $nav.height(height - 67);  // minus height of nav bar
-        $nav.addClass('add-scroll');
-        $nav.removeClass('no-scroll');
-        $('body').addClass('nav-open');
-    }
-
-    function remove_scroll(){
-        var $nav = $('body').find('[data-sv-mainnav]');
-
-        $nav.removeClass('add-scroll');
-        $nav.addClass('no-scroll');
-        $('body').removeClass('nav-open');
-    }
+    $("body").on("click", "[data-toggle-mobilenav]", function(){
+        $(".mobilenav").toggleClass("active");
+    });
